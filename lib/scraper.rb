7b44448432bd4982d12643dcee94557b7bd93e3b
LatellaL <83371530+LatellaL@users.noreply.github.com>
2021-12-04 19:37:28 -0500

    commit tree f4a0d2dab5e7fef6b9c39bdc929ea8a0ca3cf3d9

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri.HTML5(html)
+# p doc.css(".headline-26OIBN")
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
